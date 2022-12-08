aef2d9cba36c975ba8a5b6d0c55fdb3d1923a8b3
tamcy <tamcy@users.noreply.github.com>
2022-12-08 11:40:04 +0800

    cleanup & fixes

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,9 +1,19 @@
-import 'package:server/assets.dart';
+import 'package:bezier/bezier.dart';
+import 'package:vector_math/vector_math.dart';
 
 void main(List<String> args) {
   print('program started');
-  for (var i = 0; i < 100; i++) {
-    print('===== LOOP $i =====');
+
+  List<CubicBezier> beziers = [
+    CubicBezier([Vector2(419.0, 509.0), Vector2(419.0, 509.0), Vector2(419.0, 479.0), Vector2(419.0, 479.0)]),
+    CubicBezier([Vector2(419.0, 479.0), Vector2(419.0, 479.0), Vector2(684.0, 479.0), Vector2(684.0, 479.0)]),
+    CubicBezier([Vector2(419.0, 121.0), Vector2(419.0, 121.0), Vector2(714.0, 121.0), Vector2(714.0, 121.0)]),
+    CubicBezier([Vector2(714.0, 121.0), Vector2(714.0, 121.0), Vector2(714.0, 509.0), Vector2(714.0, 509.0)]),
+    CubicBezier([Vector2(789.0, -65.0), Vector2(819.0, -65.0), Vector2(837.0, -59.0), Vector2(854.0, -38.0)]),
+  ];
+
+  for (int l = 0; l < 100; l++) {
+    print('===== LOOP $l =====');
 
     for (int i = 0; i < beziers.length - 1; i++) {
       for (int j = i + 1; j < beziers.length; j++) {
@@ -14,5 +24,6 @@ void main(List<String> args) {
       }
     }
   }
+
   print('program finished successfully!');
 }
