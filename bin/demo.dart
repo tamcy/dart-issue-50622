288df718f168d536d593dbcf2707026fda50a3d7
tamcy <tamcy@users.noreply.github.com>
2022-12-08 11:23:37 +0800

    -

--- bin/demo.dart
+++ bin/demo.dart
@@ -3,7 +3,7 @@ import 'package:server/assets.dart';
 void main(List<String> args) {
   print('program started');
   for (var i = 0; i < 100; i++) {
-    print('===== LOOP ${i + 1} =====');
+    print('===== LOOP $i =====');
 
     for (int i = 0; i < beziers.length - 1; i++) {
       for (int j = i + 1; j < beziers.length; j++) {
