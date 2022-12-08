1cf250f195e5bcd936b5c3b5c189e14afef8b34e
tamcy <tamcy@users.noreply.github.com>
2022-12-08 11:19:58 +0800

    cleanup

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,12 +1,18 @@
 import 'package:server/assets.dart';
-import 'package:server/contour.dart';
 
 void main(List<String> args) {
   print('program started');
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 500; i++) {
     print('===== LOOP ${i + 1} =====');
-    final groupedContours = findOverlappingContours(loadData());
-    print(groupedContours.length);
+
+    for (int i = 0; i < beziers.length - 1; i++) {
+      for (int j = i + 1; j < beziers.length; j++) {
+        // print("  >> testing $i against $j");
+        if (beziers[i].intersectionsWithCurve(beziers[j]).isNotEmpty) {
+          print('  >>> Intersection detected for $i, $j');
+        }
+      }
+    }
   }
   print('program finished successfully!');
 }
